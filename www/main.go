392165743684ef11ebef7f88691d1b24f48c0438
Masthell <mutali.d@yandex.kz>
2024-08-11 15:41:13 +0600

    creating data model structures

--- www/main.go
+++ www/main.go
@@ -5,8 +5,26 @@ import (
 	"net/http"
 )
 
+type user struct {
+	name string
+	age uint16
+	money int64
+	avggrades float64
+	happiness float64
+}
+
+func (u user) getAllinfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d", u.name, u.age)
+}
+
+func (u *user) setNewName(newName string) {
+	u.name = newName
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	bob := user{"Bob", 25, 58, 2, 3}
+	bob.setNewName("alex")
+	fmt.Fprintf(w, bob.getAllinfo())
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +38,9 @@ func handleRequest() {
 }
 
 func main() {
+	// var bob User = ...
+	//bob := User{name: "Bob", age: 25, money: -58, avg_grades: 2.3, happiness: 3.2}
 	handleRequest()
 }
 
-// localhost:8080/ заходим через хром
+// localhost:8080
